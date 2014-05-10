e7f53c466b674c960a87f0c4311438f6ac883f02
Andrey Chizh <info.andreychizh@gmail.com>
2014-05-10 15:33:53 +0300

    Add unit tests

---
+++ test/unit/httptest.js
@@ -0,0 +1,9 @@
+
+describe('httptest', function(){
+  describe('method', function(){
+    it('should return httptest object', function(){
+      [1,2,3].indexOf(5).should.equal(-1);
+      [1,2,3].indexOf(0).should.equal(-1);
+    })
+  })
+})
